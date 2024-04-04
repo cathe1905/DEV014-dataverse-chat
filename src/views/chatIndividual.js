ae13c1afc63dc78f921f42d60a161bd996951d27
deniisolo <deniisolo@outlook.es>
2024-04-04 16:21:17 -0500

    feat: vista de chat grupal

--- src/views/chatIndividual.js
+++ src/views/chatIndividual.js
@@ -5,14 +5,14 @@ const ChatIndividual = (props) => {
   const chat = document.createElement("div");
   chat.innerHTML = `
     <section>
-      <p><</p>
+    <a href="#"> <img src="/pics/turn-back_12410927.png" alt="back"></a>
       <img src="${singer.imageUrl}" alt="">
       <p>${singer.name}</p>
     </section>
     <section>
       <input type="text" placeholder="Escribe aqui tu mensaje">
       <button type="button">
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fes%2Fpng-zfmqo&psig=AOvVaw0M0kzPOBhsSfZl10wn54TC&ust=1712267207345000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNiTpcyCp4UDFQAAAAAdAAAAABAE" alt="boton enviar mensaje">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Gato_%282%29_REFON.jpg" alt="boton enviar mensaje">
       </button>
     </section>
   `;
