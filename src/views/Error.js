8612dc304dcad17f7d574497af755d288ff36be9
catherin <catherinr24@gmail.com>
2024-04-04 18:07:35 -0300

    añadiendo estilos a home

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,6 +1,10 @@
 const Error = () => {
   const errorEl = document.createElement("div");
-  errorEl.textContent = "Error: Page not found";
+  errorEl.innerHTML = `
+  <p> Error: Page not found</p>
+  <img src="https://i.pinimg.com/736x/91/1c/0a/911c0a29688c0619d58a33da634ecef2.jpg" alt="taylor decepcionada">
+  `;
+  
   return errorEl;
 };
 
