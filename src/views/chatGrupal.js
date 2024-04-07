406169e88e7e8f6e32361d42d8d36c4d85eb4d91
deniisolo <deniisolo@outlook.es>
2024-04-06 21:48:36 -0500

    feat agregamos css grid a la vista de chatgrupal

--- src/views/chatGrupal.js
+++ src/views/chatGrupal.js
@@ -2,58 +2,90 @@ import { data } from "../data/dataset.js";
 const ChatGrupal = () => {
   // Crear el contenedor principal
   const container = document.createElement("div");
-  container.classList.add("chat-container");
+  container.classList.add("container");
 
+  const informationContainer = document.createElement("div");
+  informationContainer.classList.add("information-container");
+  container.appendChild(informationContainer);
+ 
   const backButton = document.createElement("button");
   const backButtonImage = document.createElement("img");
   backButtonImage.src =
     "https://i.pinimg.com/564x/e5/3d/bb/e53dbbf853dd8e8532650034f30ad6ed.jpg";
   backButton.appendChild(backButtonImage);
   backButton.classList.add("back-button");
+  informationContainer.appendChild(backButton);
 
-  container.appendChild(backButton);
+  const chatContainer = document.createElement("div");
+  chatContainer.classList.add("chat-container");
+  container.appendChild(chatContainer);
 
-  // Crear un div para las imágenes
-  const imagesContainer = document.createElement("div");
-  imagesContainer.classList.add("images-container");
+  const inputContainer = document.createElement("div");
+  inputContainer.classList.add("input-Container");
+  container.appendChild(inputContainer);
 
-  // Agregar las imágenes al div de imágenes
-  for (let i = 0; i < 8; i++) {
-    const image = document.createElement("img");
-    image.src = data[i].imageUrl;
-    imagesContainer.appendChild(image);
-  }
 
-  // Crear un div para las imágenes
-  const nameContainer = document.createElement("div");
-  nameContainer.classList.add("name-Container");
 
-  // Agregar los nombres al div de nombres
-  for (let i = 0; i < 8; i++) {
-    const name = document.createElement("p");
-    name.textContent = data[i].name;
-    nameContainer.appendChild(name);
-  }
+ 
 
-  // Crear el input de texto
-  const inputText = document.createElement("input");
-  inputText.setAttribute("type", "text");
-  inputText.setAttribute("placeholder", "Escribe aqui tu mensaje");
-  inputText.classList.add("message-input");
 
-  // Crear el botón con la imagen
-  const button = document.createElement("button");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = "https://cdn-icons-png.freepik.com/512/8138/8138457.png";
-  button.appendChild(buttonImage);
-  button.classList.add("send-button");
 
-  // Agregar los elementos al contenedor principal
-  container.appendChild(imagesContainer);
-  container.appendChild(inputText);
-  container.appendChild(button);
-  container.appendChild(nameContainer);
-  return container;
+  //   // Crear un div para las imágenes
+  //   const imagesContainer = document.createElement("div");
+  //   imagesContainer.classList.add("images-container");
+
+  //   // Agregar las imágenes al div de imágenes
+  //   for (let i = 0; i < data.length; i++) {
+  //     const image = document.createElement("img");
+  //     image.src = data[i].imageUrl;
+  //     imagesContainer.appendChild(image);
+  //   }
+
+  //   // Crear un div para el contenedor de Name
+  //   const nameContainer = document.createElement("div");
+  //   nameContainer.classList.add("name-Container");
+
+  //   // Agregar los nombres al div de Name
+  //   for (let i = 0; i < data.length; i++) {
+  //     const name = document.createElement("p");
+  //     name.textContent = data[i].name;
+  //     nameContainer.appendChild(name);
+  //   }
+
+  // // Crear un div para el contenedor de shortDescription
+  // const shortDescriptionContainer = document.createElement("div");
+  // shortDescriptionContainer.classList.add("shortDescription-Container");
+
+  // // Agregar los nombres al div de shortDescription
+  // for (let i = 0; i < data.length; i++) {
+  //   const shortDescription = document.createElement("p");
+  //   shortDescription.textContent = data[i].shortDescription;
+  //   shortDescriptionContainer.appendChild(shortDescription);
+  // }
+
+  //   // Crear el input de texto
+  //   const inputText = document.createElement("input");
+  //   inputText.setAttribute("type", "text");
+  //   inputText.setAttribute("placeholder", "Escribe aqui tu mensaje");
+  //   inputText.classList.add("message-input");
+
+  //   // Crear el botón con la imagen
+  //   const button = document.createElement("button");
+  //   const buttonImage = document.createElement("img");
+  //   buttonImage.src = "https://cdn-icons-png.freepik.com/512/8138/8138457.png";
+  //   button.appendChild(buttonImage);
+  //   button.classList.add("send-button");
+  
+  // informationContainer.appendChild(button);
+
+
+  //   // Agregar los elementos al contenedor principal
+  //   container.appendChild(imagesContainer);
+  //   container.appendChild(inputText);
+  
+  //   container.appendChild(nameContainer);
+  //   container.appendChild(shortDescriptionContainer);
+     return container;
 };
 
 export default ChatGrupal;
